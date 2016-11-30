3105b6d7bce4de39da1b73754fee7a93a0453473
¨glabra¨ <gigashock3u@hotmail.com>
2016-11-30 07:19:36 -0300

    Redireccion a home al hacer clic en la foto de un candidato, preparacion para el boton versus

--- src/detalle/controllers/detalle.controller.js
+++ src/detalle/controllers/detalle.controller.js
@@ -6,15 +6,21 @@
   	.module('OurDemocracyApp')
   	.controller('DetalleController',Controller);
 
-  Controller.$inject = ['$rootScope','$stateParams', 'infoCandidatoResponse'];
+  Controller.$inject = ['$rootScope','$stateParams', 'infoCandidatoResponse','$state'];
 
-  function Controller($rootScope, $stateParams, infoCandidatoResponse){
+  function Controller($rootScope, $stateParams, infoCandidatoResponse,$state){
   	
   	var vm = this;
 
   	vm.infoCandidato = infoCandidatoResponse;
 
     //$('.loading').addClass('hide');
+
+    function volverHome(){
+
+      $state.go('app.home');
+
+    }
 
     $rootScope.$broadcast('loading:false');
   }
